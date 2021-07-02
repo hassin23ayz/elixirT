575bc70322732ebec718d805f976833fee3b754b
hassin23ayz <hassin23ayz@gmail.com>
2021-07-02 16:27:55 +0600

    one test block is not isolated from another test block

--- ch07_01_todo/test/ch07_01_todo_test.exs
+++ ch07_01_todo/test/ch07_01_todo_test.exs
@@ -8,15 +8,24 @@ defmodule Ch07_01TodoTest do
   doctest Todo.Cache
 
   # the test macro is an example of metaprogramming capabilities in elixir
-  test "start()" do
-    # assert macro takes an expression and verifies it outcome
-    {result, _} = Todo.Cache.start()
-    assert :ok == result
-  end
-
-  # test "todo operations" do
-  #   {result, cache} = Todo.Cache.start()
-  #   user1 = Todo.Cache.add_elem("alice")
-
+  # test "start()" do
+  #   # assert macro takes an expression and verifies it outcome
+  #   {result, _} = Todo.Cache.start()
+  #   assert :ok == result
   # end
+
+  test "todo operations" do
+    # create a Cache
+    {:ok, cache} = Todo.Cache.start()
+    # create a User
+    Todo.Cache.add_elem("alice")
+    # get the User
+    alice = Todo.Cache.query_elem("alice")
+    # Add an entry of the User
+    Todo.Server.add_entry(alice, %{date: ~D[2021-07-02], title: "Dentist"})
+    # query the entry of the User , query result is a list of maps
+    query_entry = Todo.Server.entries(alice, ~D[2021-07-02])
+    # assert the data
+    assert [%{date: ~D[2021-07-02], id: 1, title: "Dentist"}] == query_entry
+  end
 end
